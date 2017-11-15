4399d8f3aaa5e1563c6db07db9fe316b41983c4f
calebpensica <jan_pensica@dlsu.edu.ph>
2017-11-15 18:12:11 +0800

    asdads

--- Quatro/app/src/main/java/edu/dlsu/mobapde/quatro/MainActivity.java
+++ Quatro/app/src/main/java/edu/dlsu/mobapde/quatro/MainActivity.java
@@ -3,16 +3,25 @@ package edu.dlsu.mobapde.quatro;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account);
+        setContentView(R.layout.activity_main);
+        login = (Button)findViewById(R.id.fbLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getBaseContext(), HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
 
-        Intent i = new Intent(getBaseContext(), HomeActivity.class);
-        startActivity(i);
-        finish();
     }
 }
